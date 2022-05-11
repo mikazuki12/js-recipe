63bc11e4c92fbef28c29c96ebb465a532848e294
mikazuki12 <mikazuki.00815@gmail.com>
2022-05-12 00:37:29 +0900

    human

---
+++ 04_ゼミ/week5/sample-hum/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <div class="props-example">
+      <PropsHuman
+        v-for="(person, index) in people"
+        v-bind:key="index"
+        v-bind:person="person"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PropsHuman from "@/components/PropsHuman.vue"
+
+export default {
+  components: {
+    PropsHuman,
+  },
+  data() {
+    return {
+      people: [
+        {
+          face: "😊",
+          cloth: "👕",
+        },
+        {
+          face: "😊",
+          cloth: "👚",
+        },
+        {
+          face: "😊",
+          cloth: "👗",
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+
+.slot-example,
+.props-example {
+  display: flex;
+}
+
+a {
+  cursor: default;
+  color: inherit;
+  text-decoration: none;
+}
+</style>
---
+++ 04_ゼミ/week5/sample-hum/src/components/PropsHuman.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="human">
+    <div class="atama">
+      <div>{{ person.face }}</div>
+    </div>
+    <div class="karada" v-on:click="makeLaugh">
+      <div>💪{{ person.cloth }}💪</div>
+    </div>
+    <div class="ashi">
+      <div>🦵🦵</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["person"],
+}
+</script>
+
+<style>
+.human {
+  width: 10rem;
+  font-size: 3rem;
+  text-align: center;
+}
+</style>
